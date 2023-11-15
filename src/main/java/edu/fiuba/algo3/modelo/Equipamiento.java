7ddc543677412b87a17c451d1f17e8aeb1d610e4
Lucas Jun <ljun@fi.uba.ar>
2023-11-15 18:30:23 -0300

    Caso de uso 5 y 7 en uno. Cambié el equipamiento nuevo de null a un array list vacío

--- src/main/java/edu/fiuba/algo3/modelo/Equipamiento.java
+++ src/main/java/edu/fiuba/algo3/modelo/Equipamiento.java
@@ -1,12 +1,12 @@
 package edu.fiuba.algo3.modelo;
 
 public interface Equipamiento {
-    public int usar();
+    int usar();
 }
 
 class Casco implements Equipamiento{
 
-    public int usar(){
+    int usar(){
         return (15);
     }
 }
