2eaa8672780cc91505f48a58fd1115c4ef3a5d39
Julian Rodriguez <julian.rodriguez086@gmail.com>
2023-11-23 01:20:42 -0300

    Caso de uso 9 y 12

---
+++ src/main/java/edu/fiuba/algo3/modelo/Dado.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo;
+
+public interface Dado {
+    public int tirarDado();
+}
--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -1,15 +1,42 @@
 package edu.fiuba.algo3.modelo;
-
+import java.util.Random;
 import java.util.ArrayList;
 
-public class Juego {
+public class Juego implements Dado {
 
     private int turnos;
-    private ArrayList<Gladiador> jugadores;
+    private ArrayList<Gladiador> gladiadores;
 
-    public Juego(int cantidadDeJugadores){
+    private boolean terminado;
+
+    private Tablero tablero;
+
+    public Juego(ArrayList<Gladiador> gladiadores, int cantidadDeCasillas){
         this.turnos = 0;
-        this.jugadores = new ArrayList<Gladiador>();
+        this.gladiadores = gladiadores;
+        this.terminado = false;
+        this.tablero = new Tablero(cantidadDeCasillas);
+        for (Gladiador gladiador : gladiadores) {
+            tablero.colocarMovibleEnCasilla(gladiador, 0);
+        }
+    }
 
+    public void jugarTurno() {
+        for (Gladiador gladiador : this.gladiadores) {
+            this.tablero.moverGladiador(gladiador, this.tirarDado());
+        }
+        this.turnos++;
+        if (this.turnos == 30) {
+            this.terminado = true;
+        }
+    }
+
+    @Override
+    public int tirarDado() {
+        return new Random().nextInt(6 - 1 + 1) + 1;
+    }
+
+    public boolean terminado() {
+        return this.terminado;
     }
 }
